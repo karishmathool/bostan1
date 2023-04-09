5a7a41de4ba026ea85b5f2a4f1ea59549db213b3
karishmathool <128027912+karishmathool@users.noreply.github.com>
2023-04-09 22:21:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
-    scaler.transform(np.array(data).reshape(1,-1))
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     output=regmodel.predict(final_input)[0]
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
